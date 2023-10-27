16e6d4ea4055b0f2a444893640f0d24b3c6afe4e
DVdBdev <dries.van.den.brande@proton.me>
2023-10-27 02:32:28 +0200

    protected strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,36 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dvan-den <dvan-den@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:24:34 by dvan-den          #+#    #+#             */
-/*   Updated: 2023/10/17 11:35:32 by dvan-den         ###   ########.fr       */
+/*   Updated: 2023/10/27 02:30:11 by dvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+static char	*concat_strs(const char *s1, const char *s2)
 {
-	int		i;
-	int		j;
+	size_t	len_s1;
+	size_t	len_s2;
+	size_t	i;
 	char	*str;
 
-	i = 0;
-	j = 0;
-	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!str)
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = (char *)malloc(len_s1 + len_s2 + 1);
+	if (str == NULL)
 		return (NULL);
-	while (s1[i])
+	i = 0;
+	while (i < len_s1)
 	{
-		str[i + j] = s1[i];
+		str[i] = s1[i];
 		i++;
 	}
-	while (s2[j])
+	i = 0;
+	while (i < len_s2)
 	{
-		str[i + j] = s2[j];
-		j++;
+		str[len_s1 + i] = s2[i];
+		i++;
 	}
-	str[i + j] = '\0';
+	str[len_s1 + len_s2] = '\0';
 	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	else if (s1 == NULL)
+		return (ft_strdup(s2));
+	else if (s2 == NULL)
+		return (ft_strdup(s1));
+	return (concat_strs(s1, s2));
 }
